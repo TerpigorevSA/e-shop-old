22a28c4f73164a443f156ef6601bff6373eedca2
TerpigorevSA <118421759+TerpigorevSA@users.noreply.github.com>
2025-01-31 18:57:41 +0500

    chore .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,8 @@ module.exports = (_, args) => {
     output: {
       path: dist,
       publicPath:
-      args.mode === 'development' ? `https://symmetrical-space-trout-r977r77jw693x5qq-2233.app.github.dev/` : undefined /* <- прописать данные своего github */,
-      // args.mode === 'development' ? `http://${host}:${port}/` : undefined /* <- прописать данные своего github */,
+        // args.mode === 'development' ? `https://symmetrical-space-trout-r977r77jw693x5qq-2233.app.github.dev/` : undefined /* <- прописать данные своего github */,
+        args.mode === 'development' ? `http://${host}:${port}/` : undefined /* <- прописать данные своего github */,
       filename: `js/[name].js`,
       chunkFilename: `js/[name].js`,
     },
