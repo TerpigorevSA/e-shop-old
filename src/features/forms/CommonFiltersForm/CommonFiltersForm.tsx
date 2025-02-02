6bf4ee60e7eff00ddc0be878022f19c97d65b72d
TerpigorevSA <118421759+TerpigorevSA@users.noreply.github.com>
2025-02-02 20:08:16 +0500

    chore: lint

--- src/features/forms/CommonFiltersForm/CommonFiltersForm.tsx
+++ src/features/forms/CommonFiltersForm/CommonFiltersForm.tsx
@@ -11,12 +11,12 @@ type CommonFilterFormProps<T extends CommonFilters> = {
   initialFilters: T;
   childrenSchema?: yup.ObjectSchema<Omit<T, keyof CommonFilters>>;
   children?:
-    | ReactNode
-    | ((
-        errors: Record<string, string>,
-        filters: T,
-        handleChange: <K extends keyof T>(key: K, value: T[K]) => void
-      ) => ReactNode);
+  | ReactNode
+  | ((
+    errors: Record<string, string>,
+    filters: T,
+    handleChange: <K extends keyof T>(key: K, value: T[K]) => void
+  ) => ReactNode);
 };
 
 export const CommonFiltersForm = <T extends CommonFilters>({
@@ -76,157 +76,163 @@ export const CommonFiltersForm = <T extends CommonFilters>({
     <form className={cn(styles.form)} onSubmit={handleSubmit}>
       <div>
         <label className={cn(styles.label)}>{t('CommonFiltersForm.createdAt.label')}</label>
-        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.createdAt.from')}</label>
-        <div style={{ flexDirection: 'row', display: 'flex' }}>
-          <input
-            className={cn(styles.input)}
-            type="datetime-local"
-            value={localFilters?.createdAt?.gte || ''}
-            onChange={(e) =>
-              handleChange('createdAt', {
-                ...localFilters?.createdAt,
-                gte: e.target.value,
-              })
-            }
-          />
-          <button
-            className={cn(styles.clearButton)}
-            type="button"
-            onClick={() =>
-              handleChange('createdAt', {
-                ...localFilters?.createdAt,
-                gte: undefined,
-              })
-            }
-          >
-            X
-          </button>
-        </div>
-        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.createdAt.by')}</label>
-        <div style={{ flexDirection: 'row', display: 'flex' }}>
-          <input
-            className={cn(styles.input)}
-            type="datetime-local"
-            value={localFilters?.createdAt?.lte || ''}
-            onChange={(e) =>
-              handleChange('createdAt', {
-                ...localFilters?.createdAt,
-                lte: e.target.value,
-              })
-            }
-          />
-          <button
-            className={cn(styles.clearButton)}
-            type="button"
-            onClick={() =>
-              handleChange('createdAt', {
-                ...localFilters?.createdAt,
-                lte: undefined,
-              })
-            }
-          >
-            X
-          </button>
-        </div>
+        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.createdAt.from')}
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            <input
+              className={cn(styles.input)}
+              type="datetime-local"
+              value={localFilters?.createdAt?.gte || ''}
+              onChange={(e) =>
+                handleChange('createdAt', {
+                  ...localFilters?.createdAt,
+                  gte: e.target.value,
+                })
+              }
+            />
+            <button
+              className={cn(styles.clearButton)}
+              type="button"
+              onClick={() =>
+                handleChange('createdAt', {
+                  ...localFilters?.createdAt,
+                  gte: undefined,
+                })
+              }
+            >
+              X
+            </button>
+          </div>
+        </label>
+        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.createdAt.by')}
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            <input
+              className={cn(styles.input)}
+              type="datetime-local"
+              value={localFilters?.createdAt?.lte || ''}
+              onChange={(e) =>
+                handleChange('createdAt', {
+                  ...localFilters?.createdAt,
+                  lte: e.target.value,
+                })
+              }
+            />
+            <button
+              className={cn(styles.clearButton)}
+              type="button"
+              onClick={() =>
+                handleChange('createdAt', {
+                  ...localFilters?.createdAt,
+                  lte: undefined,
+                })
+              }
+            >
+              X
+            </button>
+          </div>
+        </label>
         {errors['createdAt.lte'] && <p className={cn(styles.error)}>{t(errors['createdAt.lte'])}</p>}
       </div>
 
       <div>
         <label className={cn(styles.label)}>{t('CommonFiltersForm.updatedAt.label')}</label>
-        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.updatedAt.from')}</label>
-        <div style={{ flexDirection: 'row', display: 'flex' }}>
-          <input
-            className={cn(styles.input)}
-            type="datetime-local"
-            value={localFilters?.updatedAt?.gte || ''}
-            onChange={(e) =>
-              handleChange('updatedAt', {
-                ...localFilters?.updatedAt,
-                gte: e.target.value,
-              })
-            }
-          />
-          <button
-            className={cn(styles.clearButton)}
-            type="button"
-            onClick={() =>
-              handleChange('updatedAt', {
-                ...localFilters?.updatedAt,
-                gte: undefined,
-              })
-            }
-          >
-            X
-          </button>
-        </div>
-        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.updatedAt.by')}</label>
-        <div style={{ flexDirection: 'row', display: 'flex' }}>
-          <input
-            className={cn(styles.input)}
-            type="datetime-local"
-            value={localFilters?.updatedAt?.lte || ''}
-            onChange={(e) =>
-              handleChange('updatedAt', {
-                ...localFilters?.updatedAt,
-                lte: e.target.value,
-              })
-            }
-          />
-          <button
-            className={cn(styles.clearButton)}
-            type="button"
-            onClick={() =>
-              handleChange('updatedAt', {
-                ...localFilters?.updatedAt,
-                lte: undefined,
-              })
-            }
-          >
-            X
-          </button>
-        </div>
+        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.updatedAt.from')}
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            <input
+              className={cn(styles.input)}
+              type="datetime-local"
+              value={localFilters?.updatedAt?.gte || ''}
+              onChange={(e) =>
+                handleChange('updatedAt', {
+                  ...localFilters?.updatedAt,
+                  gte: e.target.value,
+                })
+              }
+            />
+            <button
+              className={cn(styles.clearButton)}
+              type="button"
+              onClick={() =>
+                handleChange('updatedAt', {
+                  ...localFilters?.updatedAt,
+                  gte: undefined,
+                })
+              }
+            >
+              X
+            </button>
+          </div>
+        </label>
+        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.updatedAt.by')}
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            <input
+              className={cn(styles.input)}
+              type="datetime-local"
+              value={localFilters?.updatedAt?.lte || ''}
+              onChange={(e) =>
+                handleChange('updatedAt', {
+                  ...localFilters?.updatedAt,
+                  lte: e.target.value,
+                })
+              }
+            />
+            <button
+              className={cn(styles.clearButton)}
+              type="button"
+              onClick={() =>
+                handleChange('updatedAt', {
+                  ...localFilters?.updatedAt,
+                  lte: undefined,
+                })
+              }
+            >
+              X
+            </button>
+          </div>
+        </label>
         {errors['updatedAt.lte'] && <p className={cn(styles.error)}>{t(errors['updatedAt.lte'])}</p>}
       </div>
 
       <div>
         <label className={cn(styles.label)}>{t('CommonFiltersForm.sorting.label')}</label>
-        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.sorting.byField')}</label>
-        <select
-          className={cn(styles.select)}
-          value={localFilters?.sorting?.field || ''}
-          onChange={(e) =>
-            handleChange(
-              'sorting',
-              e.target.value
-                ? {
+        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.sorting.byField')}
+          <select
+            className={cn(styles.select)}
+            value={localFilters?.sorting?.field || ''}
+            onChange={(e) =>
+              handleChange(
+                'sorting',
+                e.target.value
+                  ? {
                     field: e.target.value as Sorting['field'],
                     type: localFilters?.sorting?.type || 'ASC',
                   }
-                : undefined
-            )
-          }
-        >
-          <option value="">{t('CommonFiltersForm.sorting.field.noSorting')}</option>
-          <option value="id">{t('CommonFiltersForm.sorting.field.id')}</option>
-          <option value="createdAt">{t('CommonFiltersForm.sorting.field.createdAt')}</option>
-          <option value="updatedAt">{t('CommonFiltersForm.sorting.field.updatedAt')}</option>
-          <option value="name">{t('CommonFiltersForm.sorting.field.')}</option>
-        </select>
+                  : undefined
+              )
+            }
+          >
+            <option value="">{t('CommonFiltersForm.sorting.field.noSorting')}</option>
+            <option value="id">{t('CommonFiltersForm.sorting.field.id')}</option>
+            <option value="createdAt">{t('CommonFiltersForm.sorting.field.createdAt')}</option>
+            <option value="updatedAt">{t('CommonFiltersForm.sorting.field.updatedAt')}</option>
+            <option value="name">{t('CommonFiltersForm.sorting.field.')}</option>
+          </select>
+        </label>
         {errors['sorting.field'] && <p className={cn(styles.error)}>{t(errors['sorting.field'])}</p>}
-        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.sorting.direction')}</label>
-        <select
-          className={cn(styles.select)}
-          value={localFilters?.sorting?.type || 'ASC'}
-          onChange={(e) =>
-            handleChange('sorting', {
-              field: localFilters?.sorting?.field || 'id',
-              type: e.target.value as 'ASC' | 'DESC',
-            })
-          }
-        >
-          <option value="ASC">{t('CommonFiltersForm.sorting.type.ASC')}</option>
-          <option value="DESC">{t('CommonFiltersForm.sorting.type.DESC')}</option>
-        </select>
+        <label className={cn(styles.subLabel)}>{t('CommonFiltersForm.sorting.direction')}
+          <select
+            className={cn(styles.select)}
+            value={localFilters?.sorting?.type || 'ASC'}
+            onChange={(e) =>
+              handleChange('sorting', {
+                field: localFilters?.sorting?.field || 'id',
+                type: e.target.value as 'ASC' | 'DESC',
+              })
+            }
+          >
+            <option value="ASC">{t('CommonFiltersForm.sorting.type.ASC')}</option>
+            <option value="DESC">{t('CommonFiltersForm.sorting.type.DESC')}</option>
+          </select>
+        </label>
         {errors['sorting.type'] && <p className={cn(styles.error)}>{t(errors['sorting.type'])}</p>}
       </div>
 
