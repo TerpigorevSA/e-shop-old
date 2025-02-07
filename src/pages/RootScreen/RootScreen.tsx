eaa5473ab29f007c6781f774d333a1b2cd21fead
TerpigorevSA <118421759+TerpigorevSA@users.noreply.github.com>
2025-02-07 19:15:45 +0500

    feat: edit RootScreen

--- src/pages/RootScreen/RootScreen.tsx
+++ src/pages/RootScreen/RootScreen.tsx
@@ -5,6 +5,18 @@ import style from './RootScreen.module.css';
 const RootScreen: React.FC = () => {
   return (
     <>
+      <p>Учебный проект заготовки под магазин.</p>
+      <p>В проекте используются:</p>
+      <ul>
+        <li>React</li>
+        <li>React Router Dom</li>
+        <li>Redux</li>
+        <li>RTK Query</li>
+        <li>React Hook Form</li>
+        <li>Чистые формы для фильров</li>
+        <li>Yup</li>
+        <li>i18next</li>
+      </ul>
       <p>Есть два (минимум) зарегистрированных ползователя.</p>
       <div className={cn(style.tab)}>
         <span className={cn(style.tabItem)}>user68@example.com</span>
